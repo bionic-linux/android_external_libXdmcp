47d1195510762af8efce38806dc75adc0bdc09cc
Egbert Eich <eich@suse.de>
2004-02-26 09:22:42 +0000

    Importing vendor version xf86-4_3_99_903 on Wed Feb 26 01:21:00 PST 2004

--- DecKey.c
+++ DecKey.c
@@ -27,7 +27,7 @@ in this Software without prior written authorization from The Open Group.
  * Author:  Keith Packard, MIT X Consortium
  */
 
-/* $XFree86: xc/lib/Xdmcp/DecKey.c,v 1.4 2001/12/14 19:54:54 dawes Exp $ */
+/* $XFree86$ */
 
 #include <X11/Xos.h>
 #include <X11/X.h>
--- Wrap.c
+++ Wrap.c
@@ -27,7 +27,7 @@ in this Software without prior written authorization from The Open Group.
  * Author:  Keith Packard, MIT X Consortium
  */
 
-/* $XFree86: xc/lib/Xdmcp/Wrap.c,v 1.4 2001/12/14 19:54:56 dawes Exp $ */
+/* $XFree86$ */
 
 #include <X11/Xos.h>
 #include <X11/X.h>
