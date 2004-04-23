11ce0b3adae5dac23d95fae62570d0a7e872055f
Egbert Eich <eich@suse.de>
2004-04-23 18:43:41 +0000

    Merging XORG-CURRENT into trunk

--- Wrap.h
+++ Wrap.h
@@ -1,9 +1,10 @@
+/* $XdotOrg: Wrap.h,v 1.3 2000/08/17 19:45:50 cpqbld Exp $ */
 /* $Xorg: Wrap.h,v 1.3 2000/08/17 19:45:50 cpqbld Exp $ */
 /*
  * header file for compatibility with something useful
  */
 
-/* $XFree86: xc/lib/Xdmcp/Wrap.h,v 1.4 2003/12/19 02:05:38 dawes Exp $ */
+/* $XFree86: xc/lib/Xdmcp/Wrap.h,v 1.3 2001/01/17 19:42:44 dawes Exp $ */
 
 typedef unsigned char auth_cblock[8];	/* block size */
 
